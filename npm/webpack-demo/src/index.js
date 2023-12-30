a471f83f85cfe5f3169202c6e64bd36588f26f65
hubert22143 <33151182+hubert22143@users.noreply.github.com>
2023-12-30 13:00:49 +0100

    Exercise

--- npm/webpack-demo/src/index.js
+++ npm/webpack-demo/src/index.js
@@ -1,11 +1,36 @@
 import _ from 'lodash';
 import myName from './myName';
+import './style.css';
+import image from './icon.jpg';
+import Data from './data.xml';
+import Notes from './data.csv';
+import toml from './data.toml';
+
+import yaml from './data.yaml';
+import json from './data.json5';
+
+console.log(toml.title);
+console.log(toml.owner.name);
+
+console.log(yaml.title); 
+console.log(yaml.owner.name);
+
+console.log(json.title);
+console.log(json.owner.name);
+
  function component() {
    const element = document.createElement('div');
 
-  // Lodash, now imported by this script
    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
    element.innerHTML += ' ' + myName("Hubert");
+   element.classList.add('hello');
+   const myIcon = new Image();
+   myIcon.src = image;
+   element.appendChild(myIcon);
+
+   console.log(Data);
+   console.log(Notes);
+
    return element;
  }
  document.body.appendChild(component());
